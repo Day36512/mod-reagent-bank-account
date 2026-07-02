8aff5e1764b617a74edc7b9d42821a7821130b94
Dinkledork <118951051+Day36512@users.noreply.github.com>
2026-07-02 10:20:52 -0600

    Add tables for bank share members and invites

--- data/sql/db-characters/base/mod_reagent_bank_account_create_table.sql
+++ data/sql/db-characters/base/mod_reagent_bank_account_create_table.sql
@@ -6,3 +6,17 @@ CREATE TABLE IF NOT EXISTS `mod_reagent_bank_account` (
     `amount` int NOT NULL,
     PRIMARY KEY (`account_id`, `guid`, `item_entry`)
 ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
+
+CREATE TABLE IF NOT EXISTS `mod_reagent_bank_share_members` (
+  `member_key` INT UNSIGNED NOT NULL,
+  `owner_key` INT UNSIGNED NOT NULL,
+  `joined_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`member_key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `mod_reagent_bank_share_invites` (
+  `inviter_key` INT UNSIGNED NOT NULL,
+  `invitee_key` INT UNSIGNED NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`invitee_key`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
